ac7408040a69c742efe518c233a55072ddb5a558
subramani03 <subramanimurugan420@gmail.com>
2024-07-23 07:25:37 +0530

    Implement Redux store for Whole app

--- src/script.js
+++ src/script.js
@@ -1,48 +1,64 @@
-import React, { lazy,Suspense } from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Header";
-// import Body from "./Body";
 import AboutUs from "./AboutUs";
 import ContactUs from "./ContactUs";
 import Error from "./Error";
+import { useState } from "react";
 import ResMenu from "./ResMenu";
-import { createBrowserRouter,RouterProvider,Outlet } from "react-router-dom";
-import Shimmer from "./shimmer";
-let Body=lazy(()=>import("./Body"));
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import UserContext from "./utils/UserContext";
+import { Provider } from "react-redux";
+import AppStore from "./utils/AppStore";
+import Cart from "./Cart";
+let Body = lazy(() => import("./Body"));
 
-let App_container = () => (
-  <div className="app">
-    <Header />
-    <Outlet />
-  </div>
-);
+let App_container = () => {
+  let [userName, setUserName] = useState("Manii");
+  return (
+    <Provider store={AppStore}>
+      <div className="app">
+        <Header />
+        <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+          <Outlet />
+        </UserContext.Provider>
+      </div>
+    </Provider>
+  );
+};
 
 let RouterList = createBrowserRouter([
   {
     path: "/",
-    element: <App_container/>,
-    errorElement:<Error/>,
-    children:[
+    element: <App_container />,
+    errorElement: <Error />,
+    children: [
       {
         path: "/",
-        element: <Suspense fallback={<h1>loading..</h1>}><Body/></Suspense>,
+        element: (
+          <Suspense fallback={<h1>loading..</h1>}>
+            <Body />
+          </Suspense>
+        ),
       },
       {
         path: "/about",
-        element: <AboutUs/>,
+        element: <AboutUs />,
       },
       {
         path: "/contact",
-        element: <ContactUs/>,
+        element: <ContactUs />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
       },
       {
         path: "/restaurants/:resId",
-        element:<ResMenu/>,
+        element: <ResMenu />,
       },
-      
-    ]
+    ],
   },
- 
 ]);
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
