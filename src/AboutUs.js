5eece2f73452f9485b4e1996861eafbbee9b1120
subramani03 <subramanimurugan420@gmail.com>
2024-07-23 07:20:58 +0530

    Using context to display user name

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,5 +1,6 @@
 import User from "./User";
 import { Component } from "react";
+import UserContext from "./utils/UserContext";
 class AboutUs extends Component
 {
     constructor(props)
@@ -14,10 +15,14 @@ class AboutUs extends Component
     }
     render()
     {
-        console.log("parent render")
+        console.log("parent render")//to use context in class component w3e use <.consumer> component inside that we use callback function 
         return (
             <div className="About">
                 <h1>About User </h1>
+                <UserContext.Consumer>
+                {({loggedInUser})=>(<h4>{loggedInUser}</h4>)}
+                </UserContext.Consumer>
+               
                 <User/>
             </div>
     
--- src/Body.js
+++ src/Body.js
@@ -1,8 +1,9 @@
-import ResCard from "./ResCard";
-import { useState, useEffect } from "react";
-import Shimmer from "./shimmer";
+import ResCard,{updatedResCard} from "./ResCard";
+import { useState, useEffect, useContext } from "react";
+import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "./utils/useOnlineStatus";
+import UserContext from "./utils/UserContext";
 
 const Body = () => {
   let [listOfRestrarent, setlistOfRestrarent] = useState([]);
@@ -11,9 +12,14 @@ const Body = () => {
   useEffect(() => {
     fetchdata();
   }, []);
+  let {loggedInUser}=useContext(UserContext)
+  let PromotedResCard =updatedResCard(ResCard);
   let fetchdata = async () => {
+    // let response = await fetch(
+    //   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.1085242&lng=77.3410656&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    // );
     let response = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.1085242&lng=77.3410656&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.624480699999999&page_type=DESKTOP_WEB_LISTING"
     );
     let data = await response.json();
     let restro =
@@ -21,8 +27,9 @@ const Body = () => {
         ?.restaurants;
     setlistOfRestrarent(restro);
     setFilterRestrarent(restro);
+    console.log(data.data);
   };
-console.log(listOfRestrarent);
+
 let OnlineStatus=useOnlineStatus();
   if(OnlineStatus===false)
   {
@@ -35,14 +42,15 @@ let OnlineStatus=useOnlineStatus();
     
    );
   }
+  let {setUserName}=useContext(UserContext);
 
   return listOfRestrarent.length=== 0 ? (               //conditional-rendering
     <Shimmer />
   ) : (
     <div className="body">
-      <div className="search">
-        <input className="search-input" placeholder="search" value={searchText} onChange={(e)=>{setSearchText(e.target.value)}}/>
-        <button  onClick={()=>
+      <div className="mt-8 flex items-center justify-center gap-2">
+        <input className="px-10 outline-none py-2 w-[60%] shadow-xl rounded-[20px]" placeholder="search" value={searchText} onChange={(e)=>{setSearchText(e.target.value)}}/>
+        <button className="p-2 w-[40px]  text-white bg-orange-400 rounded-[20px]" onClick={()=>
             {
               
               let filterResSearach=listOfRestrarent.filter((res=>res.info.name.toLowerCase().includes(searchText.toLowerCase())))
@@ -52,21 +60,22 @@ let OnlineStatus=useOnlineStatus();
           <i id="search-icon" className="fa-solid fa-magnifying-glass"></i>
         </button>
         <button
-          className="filter-Btn"
+         className="p-2 w-[fit] text-nowrap text-[13px] text-white bg-orange-400 rounded-[20px]" 
           onClick={() => {
 
             let filteredRes = listOfRestrarent.filter(
-              (res) => res.info.avgRating > 4
+              (res) => res.info.avgRating > 4.3
             );
             setFilterRestrarent(filteredRes);
           }}
         >
           Top Reated Restrarent
         </button>
+        <input className="m-1 p-1 border-2 border-orange-300 rounded-md outline-none" value={loggedInUser} onChange={(e)=>{setUserName(e.target.value)}}></input>
       </div>
       <div className="res-container">
         {FilterRestrarent.map((res) => (
-          <Link  key={res.info.id} to={"/restaurants/"+res.info.id}  style={{textDecoration: 'none'}}><ResCard resData={res}/></Link>
+          <Link  key={res.info.id} to={"/restaurants/"+res.info.id}  style={{textDecoration: 'none'}}>{res.info.veg?(<PromotedResCard  resData={res}/>):(<ResCard resData={res}/>)}</Link>
         ))}
       </div>
     </div>
